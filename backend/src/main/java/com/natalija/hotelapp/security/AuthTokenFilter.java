3bbc1535b04a23cc380a3fd5597ceb23829262e6
Natalija Brankovic <brankovic.natalija01@gmail.com>
2026-03-22 22:29:11 +0100

    fix issues

--- backend/src/main/java/com/natalija/hotelapp/security/AuthTokenFilter.java
+++ backend/src/main/java/com/natalija/hotelapp/security/AuthTokenFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
